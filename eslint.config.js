10eea58bb0699906fc998b3df82c722e7f70be95
Carlos Eduardo <carlos.eduardodms1@gmail.com>
2026-09-04 21:13:48 -0300

    fix: add Node/browser globals to ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,24 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
+import globals from 'globals';
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, prettier, {
-  ignores: ['**/dist/**', '**/.turbo/**', '**/node_modules/**'],
-});
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    ignores: ['**/dist/**', '**/.turbo/**', '**/node_modules/**'],
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  {
+    files: ['spikes/monaco-worker-lang/src/**/*.ts'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.worker },
+    },
+  },
+);
